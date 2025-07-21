6252a03ca7ae568205345990009d299e085a1238
samanthaahannah <samanthaahannah@outlook.com>
2025-07-21 15:39:26 +0100

    Complete custom enumerables exercises

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,5 +1,56 @@
 module Enumerable
   # Your code goes here
+  def my_all?
+    self.all? do |elem|
+      yield(elem)
+    end
+  end
+
+  def my_any?
+    self.any? do |elem|
+      yield(elem)
+    end
+  end
+
+  def my_count
+    self.count do |elem|
+      if block_given?
+        yield(elem)
+      else
+        self.count
+      end
+    end
+  end
+
+  def my_each_with_index
+    self.each_with_index do |elem, index|
+      yield(elem, index)
+    end
+  end
+
+  def my_inject(init)
+      self.inject(init) do |accum, value|
+        yield(accum, value)  
+      end
+  end
+
+  def my_map
+    self.map do |elem|
+      yield(elem)
+    end
+  end
+
+  def my_none?
+    self.none? do |elem|
+      yield(elem)
+    end
+  end
+
+  def my_select
+    self.select do |elem|
+      yield(elem)
+    end
+  end
 end
 
 # You will first have to define my_each
@@ -8,4 +59,9 @@ end
 # to this method
 class Array
   # Define my_each here
+  def my_each
+    for item in self
+      yield(item)
+    end
+  end
 end
